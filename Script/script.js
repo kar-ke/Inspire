3b9dcea05350367f14e9b6f9dd6cd6b3a799938a
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-05-03 20:22:18 +0530

    first commit

---
+++ Script/script.js
@@ -0,0 +1,6 @@
+function handleOpen(){
+    document.querySelector(".mobile-menuitem-lists").style.display = "flex"
+}
+function handleClose(){
+    document.querySelector(".mobile-menuitem-lists").style.display = "none"
+}
